afee69cc17b266f8fffcf55f37c9938e2d40193b
Bartek <b.grabski92@gmail.com>
2013-04-19 13:43:17 +0200

    Added UserDaoImpl, added mappings to model classes in hibernate.cfg.xml

--- src/main/java/agh/bit/ideafactory/daoimpl/UserDaoImpl.java
+++ src/main/java/agh/bit/ideafactory/daoimpl/UserDaoImpl.java
@@ -1,5 +1,15 @@
 package agh.bit.ideafactory.daoimpl;
 
+import agh.bit.ideafactory.dao.UserDao;
+import agh.bit.ideafactory.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import org.hibernate.Query;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Bartek
@@ -7,5 +17,31 @@ package agh.bit.ideafactory.daoimpl;
  * Time: 13:18
  * To change this template use File | Settings | File Templates.
  */
-public class UserDaoImpl {
+
+
+/*
+ This is core class which fetches the data from the database.
+ Note that the class contains a Resource names “sessionFactory”,
+ whenever any method which contains a hibernate query is called,
+ it needs hibernate session to execute the query and
+ that session is provided by sessionFactory
+ (which is a bean provided by spring to handle the hibernate session).
+ */
+@Repository("userDao")
+public class UserDaoImpl implements UserDao {
+    @Resource
+    private SessionFactory sessionFactory;
+    public Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    @Override
+    @Transactional("transactionManager")
+    public User getUserByUserName(String username) {
+        Query queryResult;
+        queryResult = getCurrentSession().createQuery("from User where username =:username");
+        queryResult.setParameter("userName", username);
+        return (User) queryResult.list().get(0);
+    }
+
 }
