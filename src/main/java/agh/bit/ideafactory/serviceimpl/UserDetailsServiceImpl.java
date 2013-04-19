29ca1ed67f89b54c8e473470fbc3439d9de1ccec
Bartek <b.grabski92@gmail.com>
2013-04-19 14:22:36 +0200

    Added: interface UserDetailsService UserDetailsServiceImpl Modified: User - added one more method setUserAuthorities

---
+++ src/main/java/agh/bit/ideafactory/serviceimpl/UserDetailsServiceImpl.java
@@ -0,0 +1,51 @@
+package agh.bit.ideafactory.serviceimpl;
+
+import agh.bit.ideafactory.dao.UserDao;
+import agh.bit.ideafactory.model.Authority;
+import agh.bit.ideafactory.model.User;
+import agh.bit.ideafactory.service.UserDetailsService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Bartek
+ * Date: 19.04.13
+ * Time: 13:57
+ * To change this template use File | Settings | File Templates.
+ */
+@Service("userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserDao userDao;
+
+    @Override
+    public UserDetails loadUserByUserName(String username) throws UsernameNotFoundException, DataAccessException {
+        User user;
+        List<String> authorityList = new ArrayList<String>();
+        String authority;
+        try {
+            user = userDao.getUserByUserName(username);
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("getUserByUserName returned null");
+        }
+
+        Set<Authority> authoritySet = user.getAuthoritySet();
+        Iterator<Authority> authoritySetIterator = authoritySet.iterator();
+        while(authoritySetIterator.hasNext()) {
+            authority = authoritySetIterator.next().getAuthority();
+            authorityList.add(authority);
+        }
+        user.setUserAuthorities(authorityList);
+        return (UserDetails) user;
+    }
+}
