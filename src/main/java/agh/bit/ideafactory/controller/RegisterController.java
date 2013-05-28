4fbffccd7f3d696662079eb548c66830983aadc3
mateusz-ant <mateusz.anteq@gmail.com>
2013-05-28 23:37:55 +0200

    log4j logger -> slf4j logger in imports and code / addresses #8

--- src/main/java/agh/bit/ideafactory/controller/RegisterController.java
+++ src/main/java/agh/bit/ideafactory/controller/RegisterController.java
@@ -1,13 +1,13 @@
 package agh.bit.ideafactory.controller;
 
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-
-import org.apache.log4j.Logger;
+import agh.bit.ideafactory.helpers.TokenGenerator;
+import agh.bit.ideafactory.model.Token;
+import agh.bit.ideafactory.model.User;
+import agh.bit.ideafactory.service.MailService;
+import agh.bit.ideafactory.service.TokenService;
+import agh.bit.ideafactory.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -16,22 +16,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import agh.bit.ideafactory.helpers.TokenGenerator;
-import agh.bit.ideafactory.model.Token;
-import agh.bit.ideafactory.model.User;
-import agh.bit.ideafactory.service.MailService;
-import agh.bit.ideafactory.service.TokenService;
-import agh.bit.ideafactory.service.UserService;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
 
 
-@Controller 
+@Controller
 public class RegisterController {
 
-    Logger logger = Logger.getLogger(RegisterController.class);
+    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     public UserService userService;
-    
+
     @Autowired
     public MailService mailService;
 
@@ -43,14 +42,14 @@ public class RegisterController {
 
     @Autowired
     public TokenGenerator tokenGenerator;
-	
-	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public String main(ModelMap model) {
-		return "home/register";
-	}
-	
-	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String register(ModelMap model, HttpServletRequest request) {
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String main(ModelMap model) {
+        return "home/register";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String register(ModelMap model, HttpServletRequest request) {
 
         Token newToken;
         User newUser;
@@ -65,7 +64,7 @@ public class RegisterController {
                 setError(model, sb.toString());
                 return "home/register";
             }
-            newUser.setPassword(passwordEncoder.encodePassword(newUser.getPassword(),newUser.getUsername()));
+            newUser.setPassword(passwordEncoder.encodePassword(newUser.getPassword(), newUser.getUsername()));
         } catch (Exception e) {
             e.printStackTrace();
             setError(model, "Error validating user! Try again");
@@ -76,7 +75,7 @@ public class RegisterController {
 
             User result = userService.getUserByUserName(request.getParameter("username"));
             if (result != null && result.isEnabled()) {
-                setError(model,"Account exists!");
+                setError(model, "Account exists!");
                 return "home/register";
             }
 
@@ -84,17 +83,17 @@ public class RegisterController {
 
 
             if (result != null && !result.isEnabled()) {
-                    setError(model,"Account not activated - resending activation mail!");
-                    newToken.setUser(result);
-                    tokenService.saveToken(newToken);
-                    sendMail(result, newToken.getToken(), request);
-                    return "home/register";
+                setError(model, "Account not activated - resending activation mail!");
+                newToken.setUser(result);
+                tokenService.saveToken(newToken);
+                sendMail(result, newToken.getToken(), request);
+                return "home/register";
             }
 
         } catch (Exception e) {
-                logger.error("Error in fetching user");
-                setError(model,"Internal error! Please try again");
-                return "home/register";
+            logger.error("Error in fetching user");
+            setError(model, "Internal error! Please try again");
+            return "home/register";
         }
 
         //user does not exist - add new one and send mail
@@ -103,53 +102,54 @@ public class RegisterController {
             userService.addUser(newUser);
             newToken.setUser(newUser);
             tokenService.saveToken(newToken);
-            sendMail(newUser, newToken.getToken(),request);
-            setSuccess(model,"Message sent. Check your mail!");
+            sendMail(newUser, newToken.getToken(), request);
+            setSuccess(model, "Message sent. Check your mail!");
         } catch (Exception e) {
             e.printStackTrace();
-            setError(model,"Internal error! Please try again");
+            setError(model, "Internal error! Please try again");
         }
-		return "home/register";
-	}
+        return "home/register";
+    }
 
     @RequestMapping("/register/{token}")
     public String activate(@PathVariable("token") String token, ModelMap model) {
         try {
             Token result = tokenService.findToken(token);
             if (result == null) {
-                setError(model,"Token deprecated");
+                setError(model, "Token deprecated");
                 return "home/register";
             }
             User userToUpdate = result.getUser();
             userToUpdate.setEnabled(true);
             userService.update(userToUpdate);
             tokenService.deleteToken(result);
-            setSuccess(model,"Account successfully activated! Enjoy!");
+            setSuccess(model, "Account successfully activated! Enjoy!");
 
         } catch (Exception e) {
-            setError(model,"Internal error! Please try again");
+            setError(model, "Internal error! Please try again");
             e.printStackTrace();
         }
         return "home/register";
     }
+
     //-------UTILS--------
     private void sendMail(User u, String message, HttpServletRequest request) {
 
-        mailService.sendMail("from@from.pl",u.getEmail(),
-                "AGH Online Judge Registration",request.getRequestURL().append("/").append(message).toString());
+        mailService.sendMail("from@from.pl", u.getEmail(),
+                "AGH Online Judge Registration", request.getRequestURL().append("/").append(message).toString());
 
     }
 
     private User createUserFromRequest(HttpServletRequest request) {
-            User newUser = new User();
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-            String email = request.getParameter("email");
-            newUser.setUsername(username);
-            newUser.setPassword(password);
-            newUser.setEmail(email);
-            newUser.setEnabled(false);
-            return newUser;
+        User newUser = new User();
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+        newUser.setUsername(username);
+        newUser.setPassword(password);
+        newUser.setEmail(email);
+        newUser.setEnabled(false);
+        return newUser;
     }
 
     private Set<ConstraintViolation<User>> validateUser(User u) {
@@ -158,7 +158,7 @@ public class RegisterController {
     }
 
     private void hashPassword(User u) {
-        String hashed = passwordEncoder.encodePassword(u.getPassword(),u.getUsername());
+        String hashed = passwordEncoder.encodePassword(u.getPassword(), u.getUsername());
         u.setPassword(hashed);
     }
 
@@ -177,6 +177,5 @@ public class RegisterController {
         return false;
     }
 
-	
 
 }
