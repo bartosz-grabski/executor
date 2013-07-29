da4d881d46cabe098e8006188081275da2febcfa
bgrabski <b.grabski92@gmail.com>
2013-07-29 10:35:28 +0200

    Modified views Added links to business console addresses #17

--- src/main/java/agh/bit/ideafactory/controller/InstitutionConsoleController.java
+++ src/main/java/agh/bit/ideafactory/controller/InstitutionConsoleController.java
@@ -3,7 +3,6 @@ package agh.bit.ideafactory.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author bgrabski
@@ -11,8 +10,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class InstitutionConsoleController {
 
-    @RequestMapping(value="/business/console", method = RequestMethod.GET)
-    public @ResponseBody String console() {
-        return "TODO";
-    }
+	private final String viewName = "institution/institution_console";
+
+	@RequestMapping(value = "/business/console", method = RequestMethod.GET)
+	public String console() {
+		return viewName;
+	}
 }
