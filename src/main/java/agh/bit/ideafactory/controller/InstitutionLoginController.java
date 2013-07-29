67f6b7f42d6327e14bd69aa781d088e6ab4ea613
bgrabski <b.grabski92@gmail.com>
2013-07-29 10:02:30 +0200

    Moved institution views to separate folders Refactored Login/Register Controllers addresses # 17

--- src/main/java/agh/bit/ideafactory/controller/InstitutionLoginController.java
+++ src/main/java/agh/bit/ideafactory/controller/InstitutionLoginController.java
@@ -1,11 +1,11 @@
 package agh.bit.ideafactory.controller;
 
+import static agh.bit.ideafactory.helpers.ModelMapUtils.setError;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import static agh.bit.ideafactory.helpers.ModelMapUtils.setError;
 
 /**
  * @author bgrabski
@@ -13,14 +13,16 @@ import static agh.bit.ideafactory.helpers.ModelMapUtils.setError;
 @Controller
 public class InstitutionLoginController {
 
-    @RequestMapping(value="/business/login", method = RequestMethod.GET)
-    public String login(ModelMap model) {
-        return "home/institution_login";
-    }
+	private final String viewName = "institution/institution_login";
 
-    @RequestMapping(value="/business/loginfailed", method = RequestMethod.GET)
-    public String loginfailed(ModelMap model) {
-        setError(model,"true");
-        return "home/institution_login";
-    }
+	@RequestMapping(value = "/business/login", method = RequestMethod.GET)
+	public String login(ModelMap model) {
+		return viewName;
+	}
+
+	@RequestMapping(value = "/business/loginfailed", method = RequestMethod.GET)
+	public String loginfailed(ModelMap model) {
+		setError(model, "true");
+		return viewName;
+	}
 }
--- src/main/java/agh/bit/ideafactory/controller/InstitutionRegisterController.java
+++ src/main/java/agh/bit/ideafactory/controller/InstitutionRegisterController.java
@@ -1,13 +1,17 @@
 package agh.bit.ideafactory.controller;
 
+import static agh.bit.ideafactory.helpers.ModelMapUtils.setError;
+import static agh.bit.ideafactory.helpers.ModelMapUtils.setSuccess;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
 import javax.validation.Validation;
 import javax.validation.Validator;
 
-import agh.bit.ideafactory.exception.IncorrectRegisterDataException;
-import agh.bit.ideafactory.exception.PasswordMatchException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +21,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import agh.bit.ideafactory.exception.IncorrectRegisterDataException;
+import agh.bit.ideafactory.exception.PasswordMatchException;
 import agh.bit.ideafactory.model.Institution;
 import agh.bit.ideafactory.service.InstitutionService;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-import static agh.bit.ideafactory.helpers.ModelMapUtils.setError;
-import static agh.bit.ideafactory.helpers.ModelMapUtils.setSuccess;
 
 /**
  * @author bgrabski Class for handling request to create InstitutionAccount
@@ -33,15 +32,15 @@ import static agh.bit.ideafactory.helpers.ModelMapUtils.setSuccess;
 @Controller
 public class InstitutionRegisterController {
 
-	private static final String viewName = "home/institution_register";
+	private static final String viewName = "institution/institution_register";
 
-    @Autowired
+	@Autowired
 	InstitutionService institutionService;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	/**
 	 * Simply returns the view
@@ -55,73 +54,71 @@ public class InstitutionRegisterController {
 
 	@RequestMapping(value = "/business/register", method = RequestMethod.POST)
 	public String registerPost(ModelMap modelMap, HttpServletRequest request) {
-		Map<String,String> requestData = fetchRequestData(request);
+		Map<String, String> requestData = fetchRequestData(request);
 
-        try {
-            checkPasswordConfirmedProperly(requestData);
-        } catch (PasswordMatchException e) {
-            setError(modelMap,e.getMessage());
-            return viewName;
-        }
+		try {
+			checkPasswordConfirmedProperly(requestData);
+		} catch (PasswordMatchException e) {
+			setError(modelMap, e.getMessage());
+			return viewName;
+		}
 
-        Institution institution = createInstitutionFromRequestData(requestData);
+		Institution institution = createInstitutionFromRequestData(requestData);
 
-        try {
-            validateInstitution(institution);
-        } catch (IncorrectRegisterDataException e) {
-            setError(modelMap,e.getMessage());
-            return viewName;
-        }
+		try {
+			validateInstitution(institution);
+		} catch (IncorrectRegisterDataException e) {
+			setError(modelMap, e.getMessage());
+			return viewName;
+		}
 
-        try {
-            persistInstitutionToDatabase(institution);
-        } catch (Exception e) {
-            setError(modelMap, e.getMessage());
-            return viewName;
-        }
-        setSuccess(modelMap,"Acocunt created");
-        return viewName;
+		try {
+			persistInstitutionToDatabase(institution);
+		} catch (Exception e) {
+			setError(modelMap, e.getMessage());
+			return viewName;
+		}
+		setSuccess(modelMap, "Acocunt created");
+		return viewName;
 	}
 
-    private Institution createInstitutionFromRequestData(Map<String,String> requestData) {
-        Institution newInstitution = new Institution();
-        newInstitution.setUsername(requestData.get("email"));
-        newInstitution.setPassword(requestData.get("password"));
+	private Institution createInstitutionFromRequestData(Map<String, String> requestData) {
+		Institution newInstitution = new Institution();
+		newInstitution.setUsername(requestData.get("email"));
+		newInstitution.setPassword(requestData.get("password"));
 
-        newInstitution.setEnabled(true);
-        return newInstitution;
-    }
-
-    private Map<String,String> fetchRequestData(HttpServletRequest request) {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String passwordconf = request.getParameter("passwordconf");
-        Map<String,String> requestData = new HashMap<String,String>();
-        requestData.put("email",email);
-        requestData.put("password",password);
-        logger.info(requestData.get("password"));
-        requestData.put("passwordconf",passwordconf);
-        return requestData;
+		newInstitution.setEnabled(true);
+		return newInstitution;
 	}
 
-    private void persistInstitutionToDatabase(Institution institution) {
-        institutionService.addInstitution(institution);
-    }
+	private Map<String, String> fetchRequestData(HttpServletRequest request) {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String passwordconf = request.getParameter("passwordconf");
+		Map<String, String> requestData = new HashMap<String, String>();
+		requestData.put("email", email);
+		requestData.put("password", password);
+		requestData.put("passwordconf", passwordconf);
+		return requestData;
+	}
 
-    private void checkPasswordConfirmedProperly(Map<String,String> requestData) throws PasswordMatchException {
-        String password = null;
-        if ((password = requestData.get("password")) == null || !(password.equals(requestData.get("passwordconf")))) {
-            throw new PasswordMatchException("Passwords don't match");
-        }
-    }
+	private void persistInstitutionToDatabase(Institution institution) {
+		institutionService.addInstitution(institution);
+	}
 
-    private void validateInstitution(Institution institution) throws IncorrectRegisterDataException {
-        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-        Set<ConstraintViolation<Institution>> violations = validator.validate(institution);
-        if (violations.size() != 0) {
-            throw new IncorrectRegisterDataException("Incorrect input data");
-        }
-    }
+	private void checkPasswordConfirmedProperly(Map<String, String> requestData) throws PasswordMatchException {
+		String password = null;
+		if ((password = requestData.get("password")) == null || !(password.equals(requestData.get("passwordconf")))) {
+			throw new PasswordMatchException("Passwords don't match");
+		}
+	}
 
+	private void validateInstitution(Institution institution) throws IncorrectRegisterDataException {
+		Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+		Set<ConstraintViolation<Institution>> violations = validator.validate(institution);
+		if (violations.size() != 0) {
+			throw new IncorrectRegisterDataException("Incorrect input data");
+		}
+	}
 
 }
