705a4d7e7142e480da123c78af9341b13dd5842b
r2mzes <r2mzes@gmail.com>
2013-09-01 17:53:10 +0200

    minor fixes (after previous commit 36f515b suggestion)

---
+++ executor-web-app/src/main/java/agh/bit/ideafactory/exception/DirectoryCreationException.java
@@ -0,0 +1,13 @@
+package agh.bit.ideafactory.exception;
+
+public class DirectoryCreationException extends Exception {
+
+	public DirectoryCreationException(String message) {
+		super(message);
+	}
+
+	public DirectoryCreationException() {
+		super();
+	}
+
+}
