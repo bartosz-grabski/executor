2719b26d1b56c274fc484b55e0266fb3d8ac0e34
bgrabski <b.grabski92@gmail.com>
2013-09-22 01:27:38 +0200

    SubmitDao and SubmitDaoTest completed, added hibernate config for embedded database

--- executor-tester-service/src/main/java/agh/bit/ideafactory/rmi/dao/ResultDao.java
+++ executor-tester-service/src/main/java/agh/bit/ideafactory/rmi/dao/ResultDao.java
@@ -1,14 +1,20 @@
 package agh.bit.ideafactory.rmi.dao;
 
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import agh.bit.ideafactory.model.Result;
 
 @Repository("resultDao")
 public class ResultDao {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	public void putResult(Result result) {
-		
+		Criteria crit = sessionFactory.getCurrentSession().createCriteria(Result.class);
 	}
 	
 }
--- executor-tester-service/src/main/java/agh/bit/ideafactory/rmi/dao/SubmitDao.java
+++ executor-tester-service/src/main/java/agh/bit/ideafactory/rmi/dao/SubmitDao.java
@@ -1,5 +1,12 @@
 package agh.bit.ideafactory.rmi.dao;
 
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.CriteriaQuery;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.engine.spi.TypedValue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import agh.bit.ideafactory.model.Submit;
@@ -7,7 +14,17 @@ import agh.bit.ideafactory.model.Submit;
 @Repository("submitDao")
 public class SubmitDao {
 	
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 	public Submit getSubmitById(long id) {
-		return null;
+		Criteria crit = getCriteria();
+		crit.add(Restrictions.eq("id", id));
+		return (Submit) crit.uniqueResult();
+	}
+	
+	
+	private Criteria getCriteria() {
+		return sessionFactory.getCurrentSession().createCriteria(Submit.class);
 	}
 }
