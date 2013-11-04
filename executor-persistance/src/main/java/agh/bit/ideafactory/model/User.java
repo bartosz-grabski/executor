c5aa979fddbc0cdd7cb29c91d8e65237edc84857
bgrabski <b.grabski92@gmail.com>
2013-11-05 00:22:40 +0100

    EXE-17 Added hashCode() and equals() to User model class

--- executor-persistance/src/main/java/agh/bit/ideafactory/model/User.java
+++ executor-persistance/src/main/java/agh/bit/ideafactory/model/User.java
@@ -218,4 +218,29 @@ public class User implements UserDetails {
 		this.domains = domains;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+
 }
