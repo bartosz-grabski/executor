37ca202ab960f656e27da0766517a8a6f9f1b66e
bgrabski <b.grabski92@gmail.com>
2013-11-05 12:41:12 +0100

    EXE-17 Added custom annotation and argument resolver to streamline logged-in user fetching

---
+++ executor-web-app/src/main/java/agh/bit/ideafactory/helpers/ActiveUserHandlerMethodArgumentResolver.java
@@ -0,0 +1,32 @@
+package agh.bit.ideafactory.helpers;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.support.WebArgumentResolver;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import agh.bit.ideafactory.annotations.ActiveUser;
+import agh.bit.ideafactory.model.User;
+
+public class ActiveUserHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.getParameterAnnotation(ActiveUser.class) != null && parameter.getParameterType().equals(User.class);
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		if (supportsParameter(parameter)) {
+			return (User) ((Authentication) webRequest.getUserPrincipal()).getPrincipal();
+		} else {
+			return WebArgumentResolver.UNRESOLVED;
+		}
+	}
+	
+	
+
+}
